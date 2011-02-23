a9299b049d874e951396d52c7d505a46a3adafab
LokiMidgard <LokiMidgard@users.noreply.github.com>
2011-02-23 13:55:01 +0000

    Added First Test

---
+++ Midgard.UndoManager/PropertyUndoCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Midgard.WPFUndoManager
+{
+    class PropertyUndoCommand : UndoCommand
+    {
+        public PropertyUndoCommand(UndoManager manager, Object sender, PropertyInfo prop, object oldValue, object newValue)
+            : base(manager, obj => ChangePropertyValue(manager, prop, sender, newValue), obj => ChangePropertyValue(manager, prop, sender, oldValue))
+        {
+            this.Sender = sender;
+            this.NewValue = newValue;
+            this.OldValue = oldValue;
+        }
+
+        private static void ChangePropertyValue(UndoManager manager, PropertyInfo prop, Object obj, object value)
+        {
+            var t = new Tuple<Object, String, object>(obj, prop.Name, value);
+            manager.notTrackChanges.Add(t);
+            prop.SetValue(obj, value, manager.emptyArray);
+        }
+
+        public object OldValue { get; private set; }
+
+        public object NewValue { get; private set; }
+
+        public object Sender { get; private set; }
+    }
+}
---
+++ WPFUndoManagerTestProject/BasicUndoTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WPFUndoManagerTestProject
+{
+    /// <summary>
+    /// Summary description for BasicUndoTest
+    /// </summary>
+    [TestClass]
+    public class BasicUndoTest
+    {
+        public BasicUndoTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        /// <summary>
+        /// Use TestInitialize to run code before running each test 
+        /// </summary>
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            ViewModel = new BasicTestViewModel();
+        }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+
+        public BasicTestViewModel ViewModel { get; set; }
+        #endregion
+
+        [TestMethod]
+        public void UndoListIsDefaultEmpty()
+        {
+            Assert.AreEqual(0, ViewModel.UndoManager.UndoList.Count, "UndoListe muss von anfang an Leer sein.");
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
