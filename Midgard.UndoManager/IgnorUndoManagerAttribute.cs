84c04ad70018fb44083b6ef3ca62905dfc0ad99d
LokiMidgard <LokiMidgard@users.noreply.github.com>
2011-02-26 19:07:37 +0000

    Added Comments for all Public Methods in WPF UndoManager

--- Midgard.UndoManager/IgnorUndoManagerAttribute.cs
+++ Midgard.UndoManager/IgnorUndoManagerAttribute.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace Midgard.WPFUndoManager
 {
+    /// <summary>
+    /// This Attribute prevent the Monetoring of an Property.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
     public sealed class IgnorUndoManagerAttribute : Attribute
     {
