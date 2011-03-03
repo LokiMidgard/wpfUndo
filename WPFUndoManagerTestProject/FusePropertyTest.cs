80b7dd5263ab80b0dd803a92ce55c1d252731ac4
LokiMidgard <LokiMidgard@users.noreply.github.com>
2011-03-03 08:28:19 +0000

    Version 0.7

--- WPFUndoManagerTestProject/FusePropertyTest.cs
+++ WPFUndoManagerTestProject/FusePropertyTest.cs
@@ -56,9 +56,11 @@ namespace WPFUndoManagerTestProject
         public void MyTestInitialize()
         {
             ViewModel = new FusePropertyTestViewModel();
+            SecondViewModel = new FusePropertyTestViewModel();
         }
 
         FusePropertyTestViewModel ViewModel { get; set; }
+        FusePropertyTestViewModel SecondViewModel { get; set; }
         //
         // Use TestCleanup to run code after each test has run
         // [TestCleanup()]
@@ -67,7 +69,7 @@ namespace WPFUndoManagerTestProject
         #endregion
 
         [TestMethod]
-        public void TestMethod1()
+        public void TryToFuse()
         {
             var originalFuse = ViewModel.FuseInt;
             ViewModel.FuseInt++;
@@ -75,8 +77,35 @@ namespace WPFUndoManagerTestProject
             Assert.AreEqual(originalFuse + 2, ViewModel.FuseInt);
             ViewModel.UndoManager.Undo.Execute(null);
             Assert.AreEqual(originalFuse, ViewModel.FuseInt);
+        }
 
+        [TestMethod]
+        public void MayNotFuseTwoDifferentViewmodels()
+        {
+            var origionaFuse1 = ViewModel.FuseInt;
+            var origionalFuse2 = SecondViewModel.FuseInt;
 
+            Assert.AreEqual(origionaFuse1,origionalFuse2);
+            Assert.AreSame(ViewModel.UndoManager, SecondViewModel.UndoManager);
+
+            var new1 = origionaFuse1 + 1;
+            var new2 = origionalFuse2+2;
+
+            ViewModel.FuseInt = new1;
+            SecondViewModel.FuseInt = new2;
+
+            Assert.AreEqual(new1, ViewModel.FuseInt);
+            Assert.AreEqual(new2, SecondViewModel.FuseInt);
+
+            ViewModel.UndoManager.Undo.Execute(null);
+
+            Assert.AreEqual(new1, ViewModel.FuseInt);
+            Assert.AreEqual(origionalFuse2, SecondViewModel.FuseInt);
+
+            ViewModel.UndoManager.Undo.Execute(null);
+
+            Assert.AreEqual(origionaFuse1, ViewModel.FuseInt);
+            Assert.AreEqual(origionalFuse2, SecondViewModel.FuseInt);
 
 
         }
